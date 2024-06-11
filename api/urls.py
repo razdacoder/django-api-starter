4d78f2cf65b16b67b6ee08dc1f8605cc969c0331
Ramon Rasheed <ramonrash2@gmail.com>
2024-06-11 09:43:47 +0100

    Django Starter API With Auth

---
+++ api/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import include, path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/v1/auth/", include('djoser.urls')),
+    path('api/v1/auth/', include('djoser.urls.jwt')),
+    path('api/v1/auth/', include('djoser.social.urls')),
+]
